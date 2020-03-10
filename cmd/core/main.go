74021d0c50b1c878c4ff6d38ef3e04463b6ab84e
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2020-03-10 18:17:26 +0200

    Square root implemented

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	if x < 0.0 {
+		return math.NaN()
+	}
+	if x == 0.0 || x == 1.0 {
+		return x
+	}
+	z := x
+	for eps, d := 1e-10, 1.0; eps <= d; {
+		d = math.Abs(z * z - x) / 2.0 / z
+		z = math.Abs(z - d)
+		fmt.Println(z, d)
+	}
+	return z
+}
 
 func main() {
-    fmt.Println("Hello, IIO!")
+	fmt.Println(Sqrt(1.44))
 }
