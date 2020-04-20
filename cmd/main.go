07dacea8ed7b83bd966c35c80db320b5c11f89df
xXxRisingTidexXx <danylopetrik@google.com>
2020-04-20 22:53:13 +0300

    Added validation set

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"iio/pkg/loading"
-	"log"
 )
 
 func main() {
-	loader := loading.NewMNISTLoader()
-	trainingExamples, testExamples, err := loader.Load()
-	if err != nil {
-		log.Println(err)
+	if trainingSamples, validationSamples, testSamples, err := loading.NewMNISTLoader().Load(); err != nil {
+		fmt.Println(err)
 	} else {
-		log.Println(len(trainingExamples))
-		log.Println(trainingExamples[0].Activations)
-		log.Println(trainingExamples[0].Label)
-		log.Println(len(testExamples))
-		log.Println(testExamples[0].Activations)
-		log.Println(testExamples[0].Label)
+		fmt.Printf("Training set length: %d\n", len(trainingSamples))
+		fmt.Printf("Validation set length: %d\n", len(validationSamples))
+		fmt.Printf("Test set length: %d\n", len(testSamples))
 	}
 }
