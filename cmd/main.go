f2cf7ccd3ba4b539a1aa20a4f9838d35cefc6b9c
xXxRisingTidexXx <danylopetrik@google.com>
2020-04-23 00:35:08 +0300

    Adjusted main

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"iio/pkg/loading"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	if trainingSamples, validationSamples, testSamples, err := loading.NewMNISTLoader().Load(); err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Printf("Training set length: %d\n", len(trainingSamples))
-		fmt.Printf("Validation set length: %d\n", len(validationSamples))
-		fmt.Printf("Test set length: %d\n", len(testSamples))
+		fmt.Printf("Training set length: %d\n", trainingSamples.Length())
+		fmt.Printf("Validation set length: %d\n", validationSamples.Length())
+		fmt.Printf("Test set length: %d\n", testSamples.Length())
 	}
 }
