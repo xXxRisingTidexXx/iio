3c64bcbca5f9ca9e1b8fcad1fad3a0e5c93640ce
xXxRisingTidexXx <danylopetrik@google.com>
2020-04-23 23:38:22 +0300

    Specified general neuron interface

--- pkg/networks/neurons/neuron.go
+++ pkg/networks/neurons/neuron.go
@@ -2,13 +2,9 @@ package neurons
 
 import "iio/pkg/vectors"
 
-// Main network's building block, which aggregates the input
-// signal, estimates it and produces a single numeric output.
-// Generally, the main difference between various neuron's
-// implementations is the activation function, which performs
-// overall operations. It's kind depends on the functionality
-// we want to simulate.
 type Neuron interface {
-	// Bypasses the input signal to produce a scalar output.
-	Forward(vectors.Vector) float64
+	FeedForward(vectors.Vector) float64
+	Derivative(float64) float64
+	BackPropagate(float64) vectors.Vector
+	Update(vectors.Vector, float64)
 }
---
+++ pkg/networks/neurons/sigmoid.go
@@ -0,0 +1,23 @@
+package neurons
+
+import (
+	"iio/pkg/vectors"
+)
+
+type SigmoidNeuron struct{}
+
+func (neuron *SigmoidNeuron) FeedForward(activations vectors.Vector) float64 {
+	panic("implement me")
+}
+
+func (neuron *SigmoidNeuron) Derivative(activation float64) float64 {
+	panic("implement me")
+}
+
+func (neuron *SigmoidNeuron) BackPropagate(delta float64) vectors.Vector {
+	panic("implement me")
+}
+
+func (neuron *SigmoidNeuron) Update(weightDeltas vectors.Vector, biasDelta float64) {
+	panic("implement me")
+}
