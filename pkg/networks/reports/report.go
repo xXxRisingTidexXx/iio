a14ccb6c1263186fd6b3e99e5c8b5c78f5b51cbf
xXxRisingTidexXx <danylopetrik@google.com>
2020-04-23 23:38:48 +0300

    Removed extra comments

--- pkg/networks/reports/report.go
+++ pkg/networks/reports/report.go
@@ -1,6 +1,3 @@
 package reports
 
-// This object describes a classification report, which includes
-// accuracy, precision, recall and F1 score for all classes plus
-// some general metrics.
 type Report struct{}
