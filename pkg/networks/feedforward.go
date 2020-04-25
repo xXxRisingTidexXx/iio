8c0bfc9ae72fe45d2083bb4a38cbeecdfd090a2e
xXxRisingTidexXx <danylopetrik@google.com>
2020-04-26 01:42:23 +0300

    Training loop almost finished

--- pkg/networks/feedforward.go
+++ pkg/networks/feedforward.go
@@ -3,6 +3,7 @@ package networks
 import (
 	"iio/pkg/networks/guts"
 	"iio/pkg/sampling"
+	"sync"
 )
 
 type feedforwardNetwork struct {
@@ -14,17 +15,35 @@ type feedforwardNetwork struct {
 
 func (network *feedforwardNetwork) train(samples *sampling.Samples) {
 	for epoch := 0; epoch < network.epochs; epoch++ {
-		shuffled := samples.Shuffle()
-		for shuffled.Next() {
-			batch := shuffled.Batch(network.batchSize)
-
-			batch.Range(
-				func(i int, sample *sampling.Sample) {
-
-				},
-			)
+		newSamples := samples.Shuffle()
+		for newSamples.Next() {
+			batch := newSamples.Batch(network.batchSize)
+			length := batch.Length()
+			waitGroup := &sync.WaitGroup{}
+			waitGroup.Add(length)
+			deltasChannel := make(chan *guts.Deltas, length)
+			for i := 0; i < length; i++ {
+				go network.propagate(batch.Get(i), waitGroup, deltasChannel)
+			}
+			waitGroup.Wait()
+			totalDeltas := guts.NewDeltas(nil, nil)
+			for deltas := range deltasChannel {
+				totalDeltas = totalDeltas.Add(deltas)
+			}
+			totalDeltas = totalDeltas.Scale(-network.learningRate / float64(length))
+			for i, layer := range network.layers {
+				layer.Update(totalDeltas.Get(i))
+			}
 		}
 	}
+}
+
+func (network *feedforwardNetwork) propagate(
+	sample *sampling.Sample,
+	waitGroup *sync.WaitGroup,
+	deltasChannel chan *guts.Deltas,
+) {
+	defer waitGroup.Done()
 }
 
 func (network *feedforwardNetwork) validate(samples *sampling.Samples) {
