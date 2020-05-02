7ba6873f92557837def3a3de7935e8902130ecf3
Oleksiy <alexey.zhurko0@gmail.com>
2020-05-02 23:14:39 +0300

    Implemented Evaluate method

--- pkg/networks/guts/sigmoid.go
+++ pkg/networks/guts/sigmoid.go
@@ -2,12 +2,19 @@ package guts
 
 import (
 	"gonum.org/v1/gonum/mat"
+	"math"
 )
 
 type SigmoidNeuron struct{}
 
+// Calculates the elements of the incoming nodes array according
+// to the sigmoid function (1/(1+e^-x))
 func (neuron *SigmoidNeuron) Evaluate(activations mat.Vector) mat.Vector {
-	panic("implement me")
+	vector := mat.NewVecDense(activations.Len(), nil)
+	for i := 0; i < vector.Len(); i++ {
+		vector.SetVec(i, 1.0 / (1.0 + math.Pow(math.E, -activations.AtVec(i))))
+	}
+	return vector
 }
 
 func (neuron *SigmoidNeuron) Differentiate(activations mat.Vector) mat.Vector {
