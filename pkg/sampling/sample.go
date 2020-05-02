36ac0da5d65b0970aed488bd132f9e124ce458d9
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-02 14:50:53 +0300

    Added nil check in sample

--- pkg/sampling/sample.go
+++ pkg/sampling/sample.go
@@ -1,10 +1,14 @@
 package sampling
 
 import (
+	"fmt"
 	"gonum.org/v1/gonum/mat"
 )
 
 func NewSample(activations mat.Vector, label int) *Sample {
+	if activations == nil {
+		panic(fmt.Sprintf("sampling: sample activations can't be nil"))
+	}
 	return &Sample{activations, label}
 }
 
