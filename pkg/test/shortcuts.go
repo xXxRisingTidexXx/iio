5a01b998e7467b5be4f2b66bd3e9dc14f3881ea9
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-04 01:47:45 +0300

    Finished test shortcut docs

--- pkg/test/shortcuts.go
+++ pkg/test/shortcuts.go
@@ -34,11 +34,18 @@ func Spare(text string, body func()) {
 
 // Universal equality assertion expecting arguments to equal each
 // other. It differs from the standard gomega's function
-// https://pkg.go.dev/github.com/onsi/gomega?tab=doc#Equal
+// https://pkg.go.dev/github.com/onsi/gomega?tab=doc#Equal `cause it
+// leverages https://golang.org/pkg/reflect/#DeepEqual but this one
+// https://pkg.go.dev/github.com/google/go-cmp/cmp?tab=doc#Equal
+// is more preferred `cause of a higher safety and more flexible
+// customization.
 func Equate(actual interface{}, expected interface{}) {
 	gomega.Expect(cmp.Equal(actual, expected)).To(gomega.BeTrue())
 }
 
+// Universal inequality assertion based on widely-spread in our app
+// https://pkg.go.dev/github.com/google/go-cmp/cmp?tab=doc#Equal
+// equality function.
 func Discern(actual interface{}, expected interface{}) {
 	gomega.Expect(cmp.Equal(actual, expected)).To(gomega.BeFalse())
 }
