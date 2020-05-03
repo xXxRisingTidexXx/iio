8b7c748f28c45d1a1debe6baf6e473dd377e05ed
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-04 02:03:43 +0300

    Documented sample

--- pkg/sampling/sample.go
+++ pkg/sampling/sample.go
@@ -5,6 +5,8 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
+// Common example constructor. Actually, this struct needs no checks
+// except of the vector nil assertion.
 func NewSample(activations mat.Vector, label int) *Sample {
 	if activations == nil {
 		panic(fmt.Sprintf("sampling: sample activations can't be nil"))
@@ -12,15 +14,26 @@ func NewSample(activations mat.Vector, label int) *Sample {
 	return &Sample{activations, label}
 }
 
+// Simple abstraction encapsulating learning instance data. It
+// contains example's vector view and integer class label suitable
+// for a network processing. The same activations must have the same
+// labels but the same labels may refer to different vectors.
 type Sample struct {
+	// Learning object vector representation. For instance, 2D images
+	// should be flattened into 1D arrays to satisfy this struct's
+	// contract.
 	activations mat.Vector
-	label       int
+
+	// Integer class mapping.
+	label int
 }
 
+// Public vector accessor.
 func (sample *Sample) Activations() mat.Vector {
 	return sample.activations
 }
 
+// Public class mapping accessor.
 func (sample *Sample) Label() int {
 	return sample.label
 }
