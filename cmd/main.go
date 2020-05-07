379689a20a10b76a516a23ba70759ee06bd0f707
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-07 20:41:10 +0300

    Added loader call

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"iio/pkg/loading"
+)
 
+func main() {
+	_, testLoader := loading.NewMNISTLoaders()
+	sample := testLoader.Batch(10)[9]
+	for i := 0; i < 28; i++ {
+		for j := 0; j < 28; j++ {
+			x := sample.Data().AtVec(i*28 + j)
+			if x > 0.0 {
+				fmt.Print(fmt.Sprintf("%.2f ", x))
+			} else {
+				fmt.Print("     ")
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println(sample.Label())
 }
