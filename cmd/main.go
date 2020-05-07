6b2c3769c8a1be3424cf72a76d359e6f6e0a13bd
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-07 14:39:44 +0300

    End

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"iio/pkg/loading"
-	"math/rand"
-	"time"
-)
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	loader := loading.NewMNISTLoader()
-	trainingSamples, validationSamples, testSamples := loader.Load()
-	fmt.Printf("Training set length: %d\n", trainingSamples.Length())
-	fmt.Printf("Validation set length: %d\n", validationSamples.Length())
-	fmt.Printf("Test set length: %d\n", testSamples.Length())
+
 }
