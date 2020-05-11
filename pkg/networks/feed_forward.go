7986850b73b1de38afac874f12c6e26132b445a3
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-12 00:21:22 +0300

    Finished network initialization

--- pkg/networks/feed_forward.go
+++ pkg/networks/feed_forward.go
@@ -5,7 +5,7 @@ import (
 	"gonum.org/v1/gonum/mat"
 	"iio/pkg/costs"
 	"iio/pkg/init"
-	"iio/pkg/layers"
+	"iio/pkg/layered"
 	"iio/pkg/loading"
 )
 
@@ -18,7 +18,7 @@ func NewFeedForwardNetwork(
 	weightInitializer init.Initializer,
 	biasInitializer init.Initializer,
 	costFunction costs.CostFunction,
-	schemas ...layers.Schema,
+	schemas ...*layered.Schema,
 ) *FeedForwardNetwork {
 	if epochNumber < 1 {
 		panic(fmt.Sprintf("networks: invalid epoch number, %d", epochNumber))
@@ -26,18 +26,47 @@ func NewFeedForwardNetwork(
 	if batchSize < 1 {
 		panic(fmt.Sprintf("networks: invalid batch size, %d", batchSize))
 	}
-	if trainingLoader == nil || testLoader == nil {
-		panic("networks: feed forward network got nil loader(s)")
+	if trainingLoader == nil {
+		panic("networks: feed forward network training loader can't be nil")
 	}
-	return &FeedForwardNetwork{
-		epochNumber,
-		batchSize,
-		learningRate,
-		trainingLoader,
-		testLoader,
-		layers,
-		costFunction,
+	if testLoader == nil {
+		panic("networks: feed forward network test loader can't be nil")
 	}
+	if weightInitializer == nil {
+		panic("networks: feed forward network weight initializer can't be nil")
+	}
+	if biasInitializer == nil {
+		panic("networks: feed forward network bias initializer can't be nil")
+	}
+	if costFunction == nil {
+		panic("networks: feed forward network cost function can't be nil")
+	}
+	if schemas == nil {
+		panic("networks: feed forward network schemas can't be nil")
+	}
+	length := len(schemas) - 1
+	if length < 1 {
+		panic(fmt.Sprintf("networks: invalid schema number (%d) and at least 2 required", length+1))
+	}
+	layers := make([]layered.Layer, length)
+	for i, schema := range schemas {
+		if schema == nil {
+			panic(fmt.Sprintf("networks: nil schema at %d", i))
+		}
+		if i > 0 {
+			if schema.Neuron == nil {
+				panic(fmt.Sprintf("networks: input schema at %d", i))
+			}
+			layers[i-1] = layered.NewBasicLayer(
+				schema.Neuron,
+				weightInitializer.InitializeMatrix(schema.Size, schemas[i-1].Size),
+				biasInitializer.InitializeVector(schema.Size),
+			)
+		} else if schema.Neuron != nil {
+			panic("networks: the first schema must be an input one")
+		}
+	}
+	return &FeedForwardNetwork{epochNumber, batchSize, learningRate, trainingLoader, testLoader, layers, costFunction}
 }
 
 type FeedForwardNetwork struct {
@@ -46,7 +75,7 @@ type FeedForwardNetwork struct {
 	learningRate   float64
 	trainingLoader loading.Loader
 	testLoader     loading.Loader
-	layers         []layers.Layer
+	layers         []layered.Layer
 	costFunction   costs.CostFunction
 }
 
@@ -56,7 +85,7 @@ func (network *FeedForwardNetwork) Train() {
 		for network.trainingLoader.Next() {
 			batch := network.trainingLoader.Batch(network.batchSize)
 			length := len(batch)
-			deltasChannel := make(chan []*layers.Delta, length)
+			deltasChannel := make(chan []*layered.Delta, length)
 			for _, sample := range batch {
 				go network.train(sample, deltasChannel)
 			}
@@ -70,18 +99,18 @@ func (network *FeedForwardNetwork) Train() {
 	}
 }
 
-func (network *FeedForwardNetwork) train(sample *loading.Sample, deltasChannel chan<- []*layers.Delta) {
+func (network *FeedForwardNetwork) train(sample *loading.Sample, deltasChannel chan<- []*layered.Delta) {
 	length := len(network.layers)
 	activations := make([]mat.Vector, length+1)
 	activations[0] = sample.Data()
 	for i, layer := range network.layers {
 		activations[i+1] = layer.FeedForward(activations[i])
 	}
-	deltas := make([]*layers.Delta, length)
+	deltas := make([]*layered.Delta, length)
 	diffs := network.costFunction.Differentiate(activations[length], sample.Label())
 	for i := length - 1; i >= 0; i-- {
 		nodes := network.layers[i].ProduceNodes(diffs, activations[i+1])
-		deltas[i] = layers.NewDelta(nodes, activations[i])
+		deltas[i] = layered.NewDelta(nodes, activations[i])
 		if i > 0 {
 			diffs = network.layers[i].BackPropagate(nodes)
 		}
