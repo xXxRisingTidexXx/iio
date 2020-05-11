3fb8f157ffaec6dd5aab43ce944a43c469d85381
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-11 22:32:39 +0300

    Removed neuron kind

--- pkg/neurons/neuron.go
+++ pkg/neurons/neuron.go
@@ -4,17 +4,6 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-func NewNeuron(kind Kind) Neuron {
-	switch kind {
-	case Input:
-		panic("neurons: can't instantiate input neuron")
-	case Sigmoid:
-		return &SigmoidNeuron{}
-	default:
-		panic("neurons: undefined neuron kind")
-	}
-}
-
 type Neuron interface {
 	Evaluate(mat.Vector) mat.Vector
 	Differentiate(mat.Vector) mat.Vector
