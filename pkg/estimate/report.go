8c5c730da647528d8a8b114d3de4ff347788e78e
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-16 00:12:47 +0300

    Added report formatting

--- pkg/estimate/report.go
+++ pkg/estimate/report.go
@@ -1,5 +1,10 @@
 package estimate
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Report struct {
 	Classes  []*Record
 	MacroAvg *Record
@@ -7,5 +12,33 @@ type Report struct {
 }
 
 func (report *Report) String() string {
-	return ""
+	builder := strings.Builder{}
+	builder.WriteString("           | precision | recall | f1-score | support \n")
+	builder.WriteString("-----------+-----------+--------+----------+---------\n")
+	for i, class := range report.Classes {
+		builder.WriteString(
+			fmt.Sprintf(
+				" class %3d | %9.3f | %6.3f | %8.3f | %7d \n",
+				i,
+				class.Precision,
+				class.Recall,
+				class.F1Score,
+				class.Support,
+			),
+		)
+	}
+	builder.WriteString(
+		fmt.Sprintf(
+			" macro avg | %9.3f | %6.3f | %8.3f | %7d \n",
+			report.MacroAvg.Precision,
+			report.MacroAvg.Recall,
+			report.MacroAvg.F1Score,
+			report.MacroAvg.Support,
+		),
+	)
+	builder.WriteString("-----------+-----------+--------+----------+---------\n")
+	builder.WriteString(
+		fmt.Sprintf("  accuracy |                                   %5.3f \n\n", report.Accuracy),
+	)
+	return builder.String()
 }
