802f7e2c742a867c321ea75bc01fc45d8335db74
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-15 13:48:36 +0300

    Changed panics

--- pkg/loading/mnist.go
+++ pkg/loading/mnist.go
@@ -6,21 +6,23 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 func NewMNISTLoaders() (*MNISTLoader, *MNISTLoader) {
-	projectPath, err := filepath.Abs(".")
-	if err != nil {
-		panic(fmt.Errorf("loading: mnist didn't establish project path, %v", err))
+	_, filePath, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("loading: root dir wasn't instantiated")
 	}
+	rootDir := filepath.Dir(filepath.Dir(filepath.Dir(filePath)))
 	return newMNISTLoader(
-			filepath.Join(projectPath, "data", "train-images-idx3-ubyte.idx"),
-			filepath.Join(projectPath, "data", "train-labels-idx1-ubyte.idx"),
+			filepath.Join(rootDir, "data", "train-images-idx3-ubyte.idx"),
+			filepath.Join(rootDir, "data", "train-labels-idx1-ubyte.idx"),
 			60000,
 		),
 		newMNISTLoader(
-			filepath.Join(projectPath, "data", "t10k-images-idx3-ubyte.idx"),
-			filepath.Join(projectPath, "data", "t10k-labels-idx1-ubyte.idx"),
+			filepath.Join(rootDir, "data", "t10k-images-idx3-ubyte.idx"),
+			filepath.Join(rootDir, "data", "t10k-labels-idx1-ubyte.idx"),
 			10000,
 		)
 }
@@ -64,10 +66,10 @@ func (loader *MNISTLoader) Next() bool {
 
 func (loader *MNISTLoader) Batch(size int) []*Sample {
 	if size < 1 {
-		panic(fmt.Errorf("loading: mnist got too low batch size %d", size))
+		panic(fmt.Sprintf("loading: mnist got too low batch size %d", size))
 	}
 	if loader.position >= loader.length {
-		panic(fmt.Errorf("loading: mnist batching ended"))
+		panic(fmt.Sprintf("loading: mnist batching ended"))
 	}
 	if difference := loader.length - loader.position; difference < size {
 		size = difference
@@ -99,34 +101,34 @@ func (loader *MNISTLoader) readIDX(size int) ([][]byte, [][]byte) {
 	images, labels := make([][]byte, size), make([][]byte, size)
 	imagesFile, err := os.Open(loader.imageFilePath)
 	if err != nil {
-		panic(fmt.Errorf("loading: mnist images didn't open, %v", err))
+		panic(fmt.Sprintf("loading: mnist images didn't open, %v", err))
 	}
 	labelsFile, err := os.Open(loader.labelFilePath)
 	if err != nil {
-		panic(fmt.Errorf("loading: mnist labels didn't open, %v", err))
+		panic(fmt.Sprintf("loading: mnist labels didn't open, %v", err))
 	}
 	for i := 0; i < size; i++ {
 		images[i], labels[i] = make([]byte, loader.imageExtent), make([]byte, loader.labelExtent)
 		read, err := imagesFile.ReadAt(images[i], int64(loader.imageShift+loader.imageExtent*indices[i]))
 		if err != nil {
-			panic(fmt.Errorf("loading: mnist image reading failed, %v", err))
+			panic(fmt.Sprintf("loading: mnist image reading failed, %v", err))
 		}
 		if read != loader.imageExtent {
-			panic(fmt.Errorf("loading: mnist image extent mismatch, %d", read))
+			panic(fmt.Sprintf("loading: mnist image extent mismatch, %d", read))
 		}
 		read, err = labelsFile.ReadAt(labels[i], int64(loader.labelShift+loader.labelExtent*indices[i]))
 		if err != nil {
-			panic(fmt.Errorf("loading: mnist label reading failed, %v", err))
+			panic(fmt.Sprintf("loading: mnist label reading failed, %v", err))
 		}
 		if read != loader.labelExtent {
-			panic(fmt.Errorf("loading: mnist label extent mismatch, %d", read))
+			panic(fmt.Sprintf("loading: mnist label extent mismatch, %d", read))
 		}
 	}
 	if err := imagesFile.Close(); err != nil {
-		panic(fmt.Errorf("loading: mnist images didn't close, %v", err))
+		panic(fmt.Sprintf("loading: mnist images didn't close, %v", err))
 	}
 	if err := labelsFile.Close(); err != nil {
-		panic(fmt.Errorf("loading: mnist labels didn't close, %v", err))
+		panic(fmt.Sprintf("loading: mnist labels didn't close, %v", err))
 	}
 	return images, labels
 }
