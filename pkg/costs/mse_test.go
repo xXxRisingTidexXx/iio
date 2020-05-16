f8ccadcb8f55c36f4be6a2b24aee4470af4e79e9
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-17 00:12:10 +0300

    Added mse cost test cases

--- pkg/costs/mse_test.go
+++ pkg/costs/mse_test.go
@@ -2,7 +2,6 @@ package costs_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"iio/pkg/costs"
 	"iio/pkg/test"
 )
@@ -11,9 +10,18 @@ var _ = Describe("mse", func() {
 	costFunction := costs.NewMSECostFunction()
 	Context("evaluation", func() {
 		test.With("should correctly calculate the final class among two equal ones", func() {
-			Expect(
+			test.Comply(
 				costFunction.Evaluate(test.Vector(0, 0.003, 0.027, 0.21, 0.308, 0.308, 0.0004, 0.00037)),
-			).To(Equal(4))
+				4,
+			)
+		})
+	})
+	Context("cost", func() {
+		test.With("should correctly calculate the final output cost", func() {
+			test.Comply(
+				costFunction.Cost(test.Vector(0.001, 0.00032, 0.472, 0.1102, 0.63118), 4),
+				0.37095733480000004,
+			)
 		})
 	})
 	Context("differentiation", func() {
