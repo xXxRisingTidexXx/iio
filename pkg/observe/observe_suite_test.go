d0dfb1474cda090404c39438857ef40e5b692cbf
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-17 15:22:04 +0300

    Added observe test suite

---
+++ pkg/observe/observe_suite_test.go
@@ -0,0 +1,12 @@
+package observe_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestObserve(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "observe suite")
+}
